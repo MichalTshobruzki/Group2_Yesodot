452587b97325f2ae7a71971b557fadfe16d71045
MichalTshobruzki <45293141+MichalTshobruzki@users.noreply.github.com>
2019-01-01 12:50:20 +0200

    Add files via upload

---
+++ final project/test_project.py
@@ -0,0 +1,34 @@
+import unittest
+import io
+import project
+# import unittest.mock
+
+
+class TestProject(unittest.TestCase):
+    def test_max_val(self):
+        self.assertEqual(5, project.max_val([1, 2, 3, 4, 5]))
+        self.assertIsNot(4, project.max_val([1, 2, 3, 4, 5]))
+
+    def test_check_validation_of_product_code(self):
+        self.assertIs(False, project.check_validation_of_product_code(1234))
+        self.assertIs(False, project.check_validation_of_product_code(666))
+        self.assertIs(True, project.check_validation_of_product_code(1))
+
+    def test_build_one_shift(self):
+        result = project.build_one_shift(1, 1)
+        self.assertIn('tair', result)
+
+    # def test_build_one_shift1(self):
+    #     result = project.build_one_shift(0, 1)
+    #     self.assertIn('Sunday', result)
+
+    def test_make_shifts_for_shift_manager(self):
+        # result = project.make_shifts_for_shift_manager([['michal', [1, 1], [2, 6]], ['emilia', [1, 6], [2, 1]]])
+        self.assertIn('michal', project.make_shifts_for_shift_manager(
+            [['michal', [1, 1], [2, 6]], ['emilia', [1, 6], [2, 1]]])[0])
+        self.assertIn('emilia', project.make_shifts_for_shift_manager(
+            [['michal', [1, 1], [2, 6]], ['emilia', [1, 6], [2, 1]]])[0])
+
+if __name__ == '__test_project__':
+    unittest.main()
+
